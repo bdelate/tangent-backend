c7fb2c5ac748ff7534b1ec680092382799536287
bdelate <brendon.delate@gmail.com>
2018-07-15 15:57:42 +0200

    Added create and update methods to employee serializer to ensure that passwords are encoded correctly using set_password

--- backend/employees/serializers.py
+++ backend/employees/serializers.py
@@ -4,7 +4,8 @@ from rest_framework import serializers
 
 class EmployeeSerializer(serializers.HyperlinkedModelSerializer):
     url = serializers.HyperlinkedIdentityField(
-        view_name='employees:employee-detail')
+        view_name='employees:employee-detail'
+    )
 
     class Meta:
         model = employee_models.Employee
@@ -14,6 +15,9 @@ class EmployeeSerializer(serializers.HyperlinkedModelSerializer):
             'password',
             'first_name',
             'last_name',
+            'is_active',
+            'is_staff',
+            'is_superuser',
             'email',
             'rank',
             'cell_phone',
@@ -25,3 +29,42 @@ class EmployeeSerializer(serializers.HyperlinkedModelSerializer):
             'password': {'write_only': True,
                          'style': {'input_type': 'password'}}
         }
+
+    def create(self, validated_data: dict) -> employee_models.Employee:
+        """
+        Create an employee and encode password using set_password
+
+        Args:
+            validated_data: validated serializer data
+
+        Returns:
+            Employee instance
+        """
+        password = validated_data.pop('password')
+        user_instance = employee_models.Employee(**validated_data)
+        user_instance.set_password(password)
+        user_instance.save()
+        return user_instance
+
+    def update(self,
+               instance: employee_models.Employee,
+               validated_data: dict) -> employee_models.Employee:
+        """
+        Update an employee and encode updated password if present
+
+        Args:
+            instance: existing employee instance
+            validated_data: validated serializer data
+
+        Returns:
+            the updated employee instance
+        """
+        password = validated_data.pop('password', None)
+        if password is not None:
+            instance.set_password(password)
+
+        for field, value in validated_data.items():
+            setattr(instance, field, value)
+
+        instance.save()
+        return instance
