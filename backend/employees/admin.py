2ad62abfedfeba3e95a83ee9ede1aa0f6516b830
bdelate <brendon.delate@gmail.com>
2018-07-15 13:38:32 +0200

    Added Employee model fields to standard User fields present in the django admin.

--- backend/employees/admin.py
+++ backend/employees/admin.py
@@ -1,1 +1,16 @@
 from django.contrib import admin
+from .models import Employee
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+    """
+    Ensure Employee model is included with User details within Django admin
+    """
+
+    list_display = ('username', 'first_name', 'last_name', 'rank')
+
+    class Meta:
+        model = Employee
+
+
+admin.site.register(Employee, EmployeeAdmin)
