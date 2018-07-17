1c12dcda617a03baac1f51c70924c7e92d778f57
bdelate <brendon.delate@gmail.com>
2018-07-17 10:46:28 +0200

    Implemented JWT authorisation mechanism in addition to existing session authentication.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import datetime
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = '0hgf9pehhh_997nd32i56iotpc76%4^pny)@vp%g1um(cy%qm%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -125,9 +126,15 @@ AUTH_USER_MODEL = 'employees.Employee'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     )
+}
+
+# 30 day expiration for JWT auth tokens
+JWT_AUTH = {
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=30)
 }
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     path('api/', include('employees.urls', namespace='employees')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('obtain-auth-token/', obtain_jwt_token),
     path('admin/', admin.site.urls),
 ]
