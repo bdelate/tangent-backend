ae18621b3f3dc7c320f7609c581bfa318cc472b8
bdelate <brendon.delate@gmail.com>
2018-07-18 17:39:05 +0200

    Added ordering and string representation to Employee model.

--- backend/employees/models.py
+++ backend/employees/models.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 
 class Employee(AbstractUser):
+    """
+    Employee which inherits all default attributes from Django's builtin User
+    """
     RANK_TYPES = (
         ('Management', 'Management'),
         ('Senior', 'Senior'),
@@ -12,3 +15,9 @@ class Employee(AbstractUser):
     rank = models.CharField(max_length=12, null=True, choices=RANK_TYPES)
     cell_phone = models.CharField(max_length=10, null=True)
     salary = models.FloatField(null=True)
+
+    class Meta:
+        ordering = ['first_name']
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
